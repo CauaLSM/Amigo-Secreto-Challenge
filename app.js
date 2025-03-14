f0854b16ab3819ee182bb6eed16fa13f2b6cf64d
Cauã <caualuis2012@hotmail.com>
2025-03-14 13:33:30 -0300

    adicionar função adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    nome = document.getElementById('amigo').value
+    if(nome == ''){
+        alert('Por favor, digite um nome')
+    } else{
+        amigos.push(nome);
+        document.getElementById('amigo').value = '';
+    }
+}
+
