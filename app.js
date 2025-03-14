6550c1ea4f34dc68036132fa3663ec3e65e85bc4
Cauã <caualuis2012@hotmail.com>
2025-03-14 13:56:56 -0300

    adicionar funções para mostrar os amigos, sortear e mostrar o resultado.

--- app.js
+++ app.js
@@ -9,5 +9,29 @@ function adicionarAmigo(){
         amigos.push(nome);
         document.getElementById('amigo').value = '';
     }
+    console.log(amigos);
+    mostrarAmigos();
+}
+
+function mostrarAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++){
+        let itemLista = document.createElement('li');
+        itemLista.innerHTML = amigos[i];
+        listaAmigos.appendChild(itemLista);
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length < 2){
+        alert('Adicione mais amigos para sortear');
+    } else{
+        let indiceAmigoSorteado = Math.floor(parseInt(Math.random() * amigos.length));
+        let amigoSorteado = amigos[indiceAmigoSorteado];
+        let elementoResultado = document.getElementById('resultado');
+        elementoResultado.innerHTML = amigoSorteado;
+    }
 }
 
